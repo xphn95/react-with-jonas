8b93803750291d42692be15b02fbf1118ae1ce0e
小屁孩 <66484849+xphn95@users.noreply.github.com>
2024-11-13 09:36:05 +0000

    第一节完成

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 export default function App() {
   const [advice, setAdvice] = useState("")
+  const [count, setCount] = useState(0)
   async function getAdvice() {
     const res = await fetch("https://api.adviceslip.com/advice")
     const data = await res.json()
     setAdvice(data.slip.advice)
+    setCount((c) => c + 1)
   }
+
+  // useEffect 包含两个参数，一个初始化执行函数， 一个依赖数组
+  useEffect(function () {
+    getAdvice()
+  }, [])
   return (
     <div>
       <h1>{advice}</h1>
       <button onClick={getAdvice}>Get advice</button>
+      <Message count={count} />
     </div>
+  )
+}
+
+function Message(props) {
+  return (
+    <p>
+      You have read <strong>{props.count}</strong> pieces of advice.
+    </p>
   )
 }
